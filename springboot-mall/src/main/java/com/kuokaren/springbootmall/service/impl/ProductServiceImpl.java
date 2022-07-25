5e099809e0ec22e3adffd873867f52cd54375db3
KarenKuo7 <karenkuo199564@gmai.com>
2022-07-25 23:13:20 +0800

    商品功能 － 單元測試

--- springboot-mall/src/main/java/com/kuokaren/springbootmall/service/impl/ProductServiceImpl.java
+++ springboot-mall/src/main/java/com/kuokaren/springbootmall/service/impl/ProductServiceImpl.java
@@ -28,6 +28,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product getProductById(Integer productId) {
+
         return productDao.getProductById(productId);
     }
 
